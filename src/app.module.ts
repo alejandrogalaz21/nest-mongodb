4785d26526e9ddc934727f87b65e7b5b2aabd159
Jesus Alejandro Galaz Icedo <jesus.galaz@softtek.com>
2024-03-21 20:52:11 -0700

    create pokemon resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { ServeStaticModule } from '@nestjs/serve-static'
 import { join } from 'path'
+import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'public') }),
+    PokemonModule,
   ],
   controllers: [],
   providers: [],
