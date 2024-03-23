52c29372cea25d4adef0c8779566c4f57a5dac5c
Jesus Alejandro Galaz Icedo <jesus.galaz@softtek.com>
2024-03-23 16:40:06 -0700

    solucione el problema de acceder a os valores de config env dentro de un modulo importado

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,24 @@
 import { join } from 'path'
 import { Module } from '@nestjs/common'
-import { ConfigModule } from '@nestjs/config'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 import { ServeStaticModule } from '@nestjs/serve-static'
+import { EnvConfiguration } from './config/configuration'
+// modules
 import { MongooseModule } from '@nestjs/mongoose'
 import { PokemonModule } from './pokemon/pokemon.module'
 import { CommonModule } from './common/common.module'
 import { SeedModule } from './seed/seed.module'
-import { EnvConfiguration } from './config/configuration'
-
-const mongodbUrl = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@localhost:27017/nest?authSource=admin`
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, load: [EnvConfiguration] }),
     ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'public') }),
-    MongooseModule.forRoot(mongodbUrl),
+    MongooseModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('db.url')
+      }),
+      inject: [ConfigService]
+    }),
     PokemonModule,
     CommonModule,
     SeedModule
@@ -23,8 +27,7 @@ const mongodbUrl = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@
   providers: []
 })
 export class AppModule {
-  constructor() {
-    const { DB_USER, DB_PASSWORD, PORT } = process.env
-    console.log({ DB_USER, DB_PASSWORD, PORT })
-  }
+  // constructor(private configService: ConfigService) {
+  //   console.log(this.configService.get<string>('db.url'))
+  // }
 }
