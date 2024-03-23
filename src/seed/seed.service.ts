186c656621a4786ee0e662c4d029a4d1194613ef
Jesus Alejandro Galaz Icedo <jesus.galaz@softtek.com>
2024-03-23 12:37:33 -0700

    cree la logica del seed service para consultar el api de pokemon

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@nestjs/common'
+import axios, { AxiosInstance } from 'axios'
+import { PokeResponse } from './interfaces'
 
 @Injectable()
 export class SeedService {
-  executeSeed() {
-    return 'executed'
+  private readonly axios: AxiosInstance = axios
+
+  async executeSeed() {
+    try {
+      const { data } = await this.axios.get<PokeResponse>(
+        'https://pokeapi.co/api/v2/pokemon?limit=20'
+      )
+
+      data.results.forEach(({ name, url }) => {
+        const segments = url.split('/')
+        const no = parseInt(segments[segments.length - 2])
+        console.log({ name, no })
+      })
+
+      return 'executed'
+    } catch (error) {
+      console.log(error)
+    }
   }
 }
